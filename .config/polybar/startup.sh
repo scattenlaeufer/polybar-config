e2616644b72e9cfb120659030a2a9bf72d566279
Björn Guth <bjoern.guth@aixigo.com>
2023-09-03 12:46:16 +0200

    Create a starup script for polybar

---
+++ .config/polybar/startup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload "$(hostname --fqdn | cut -d "." -f 1)" &
+  done
+else
+  polybar --reload main &
+fi
